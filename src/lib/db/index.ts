e690ee2502c1d04f2ecd334feac0c687cb428f09
roninlp <ronin.lp@gmail.com>
2025-08-22 12:46:29 +0330

    feat: implement authentication and database setup with better-auth and drizzle-orm

---
+++ src/lib/db/index.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+import { env } from "@/env";
+
+const client = createClient({
+  url: env.DATABASE_URL,
+  authToken: env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle({
+  client,
+  casing: "snake_case",
+});
